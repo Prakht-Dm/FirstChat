90fb8c9e1c1689beaa31e6f9d8e317018e42a109
DMITRY PRAKHT <98157604+Prakht-Dm@users.noreply.github.com>
2022-04-30 17:40:18 +0700

    stable version

--- layout.js
+++ layout.js
@@ -1,6 +1,14 @@
 function switchBlur(){
-    blurBlock.style.display = (blurBlock.style.display === 'none') ? 'block' : 'none'; 
+    blurBlock.style.display = (blurBlock.style.display === 'block') ? 'none' : 'block'; 
+    settingsBlock.style.display = (settingsBlock.style.display === 'flex') ?'none' : 'flex';
+    codePage.style.display = 'none';
+}
+function closeLogginPageAndOpenCodePage(){
+    logginPage.style.display = 'none';
+    codePage.style.display = 'flex'
 }
 settingsButton.addEventListener("click", switchBlur);
 leaveSettings.addEventListener("click", switchBlur);
+leaveCode.addEventListener("click", switchBlur);
+leaveLoggin.addEventListener("click", closeLogginPageAndOpenCodePage);
 
--- main.js
+++ main.js
@@ -1,10 +1,45 @@
+import format from 'date-fns/format'
+
 function addNewMessage(event){
     event.preventDefault();
-    if (this.firstElementChild.value){
     let message = this.firstElementChild.value;
+    if (message & message.trim() !== ''){
+    let message = this.firstElementChild.value;
+    let messageTime=format(new Date(), 'HH:mm');
+    
     this.firstElementChild.value = "";
-    chatBlock.append(userMessage.content.cloneNode(true));
-    chatBlock.lastElementChild.firstElementChild.innerHTML = `${message}<p>TIME</p>`;
+
+    chatBlock.prepend(userMessage.content.cloneNode(true));
+    chatBlock.firstElementChild.innerHTML = `<div>Я: ${message}<p>${messageTime}</p></div>`;   
 }
+}
+function pressBtn(event){
+    event.preventDefault();
+    let emailAdress=inputEmail.value;  
+    inputEmail.value="";
+    const requestSwe = getEmailAndSendPost(emailAdress);
+    
+    logginPage.style.display = 'none';
+    codePage.style.display = 'flex'
+}
+async function getEmailAndSendPost(emailAdress){
+    try{
+
+    return fetch('https://mighty-cove-31255.herokuapp.com/api/user',{
+    method: "POST", 
+    headers: {
+        'content-type' : 'application/json;charset=utf-8',}, 
+    body: JSON.stringify({email: emailAdress})});
+
+
+    }
+    catch(error){
+        alert(error);
+    }
 }
 sendMessage.addEventListener("submit", addNewMessage);
+emailForm.addEventListener('submit', pressBtn);
+
+
+document.cookie = "user=John";
+alert( document.cookie );
